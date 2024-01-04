d3cffb6dc29ab7120f39b3c8a437899ad3797278
Tasnoor Jahan Dipi <145858754+Dipi0322@users.noreply.github.com>
2024-01-05 01:24:38 +0600

    Update fuel.py

--- pset 3/fuel.py
+++ pset 3/fuel.py
@@ -1,22 +1,20 @@
-from fractions import Fraction
-
 while True:
     try:
-        x = input("Fraction:")
-        num,denom = x.split("/")
-        if int(num) > int(denom):
-            print("Invalid prompt")
-            continue
-        z = Fraction(x)
-        y = z*100
-        y = round(y)
-        if y > 1 and y < 99:
-            print(f"{y}%")
-        elif y <= 1:
+        fraction = input("x/y: ")
+        num,denom = fraction.split("/")
+        num,denom = int(num),int(denom)
+
+        percentage = round((num/denom)*100)
+
+        if 1 < percentage < 99:
+            print(f"{percentage}%")
+            break
+        elif percentage <= 1:
             print("E")
-        elif 99 <= y <= 100:
+            break
+        elif 100 >= percentage >= 99:
             print("F")
+            break
+
     except (ValueError,ZeroDivisionError):
         pass
-    else:
-        break
